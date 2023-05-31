023d1bbdbb39a856e6959f1116d5846b1c9674fc
Rupert Akomeah Sakyi <rupertsakyi@Ruperts-MacBook-Pro.local>
2023-05-31 17:29:31 +0000

    first commit

---
+++ Message/app.js
@@ -0,0 +1,28 @@
+// Selectors
+const msgInput = document.querySelector(".msg-input");
+const msgButton = document.querySelector(".msg-button");
+const msgList = document.querySelector(".msg-list");
+
+//Event Lsiteners
+msgButton.addEventListener("click", showMsg);
+const array = [];
+
+//functions
+function showMsg(e) {
+  e.preventDefault();
+
+  //ShowDiv
+  const showDiv = document.createElement("div");
+  showDiv.classList.add("message");
+
+  //ShowLi
+  const showLi = document.createElement("li");
+  showLi.innerHTML = msgInput.value;
+  array.push(msgInput.value);
+  showLi.classList.add("messageList");
+  showDiv.appendChild(showLi);
+
+  //Append to message container
+  msgList.appendChild(showDiv);
+  msgInput.value = "";
+}
