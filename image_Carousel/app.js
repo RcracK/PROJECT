3206f74a85641b1c014635173fefb0a79555c792
Rupert Akomeah Sakyi <rupertsakyi@Ruperts-MacBook-Pro.local>
2023-06-04 23:16:38 +0000

    update

--- image_Carousel/app.js
+++ image_Carousel/app.js
@@ -2,31 +2,35 @@
 const images = document.querySelectorAll(".image");
 const previous = document.querySelector(".prev");
 const next = document.querySelector(".next");
-const dots = document.querySelector(".dots");
+const dots = document.querySelector(".dot");
 
 // Event Listeners
 previous.addEventListener("click", showPrevImg);
 next.addEventListener("click", showNextImg);
 
 //Functions
-showImg();
 let index = 0;
+showImg();
 function showImg() {
   for (i = 0; i < images.length; i++) {
     if (i === index) {
       images[i].style.display = "block";
+      dots[i].classList.add("active");
     } else {
       images[i].style.display = "none";
+      dots[i].classList.remove("active");
     }
   }
 }
 function showPrevImg() {
   index--;
+
   if (index < 0) {
     index = images.length - 1;
   }
   showImg();
 }
+
 function showNextImg() {
   index++;
   if (index > images.length - 1) {
