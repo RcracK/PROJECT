7c2c5b868bdacedfa4a8fd398e053310df6b2978
RCRACK <crackcodec08@gmail.com>
2023-09-17 23:06:30 +0000

    MAths

---
+++ Math4kids/divide.js
@@ -0,0 +1,67 @@
+//Selectors
+
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const audio = document.getElementById("myAudio");
+let answer = 0;
+let num1 = 0;
+let num2 = 0;
+
+//Event Listeners
+option1.addEventListener("click", function () {
+  if (option1.innerHTML == answer) {
+    generate_equation();
+  } else {
+    audio.play();
+  }
+});
+option2.addEventListener("click", function () {
+  if (option2.innerHTML == answer) {
+    generate_equation();
+  } else {
+    audio.play();
+  }
+});
+option3.addEventListener("click", function () {
+  if (option3.innerHTML == answer) {
+    generate_equation();
+  } else {
+    audio.play();
+  }
+});
+
+//Functions
+
+function generate_equation() {
+  num1 = Math.abs(Math.floor(Math.random() * 15));
+  num2 = Math.abs(Math.floor(Math.random() * 15));
+  let dummy1 = Math.floor(Math.random() * 15);
+  let dummy2 = Math.floor(Math.random() * 15);
+  let allAnswers = [];
+  let switchAnswers = [];
+
+  answer = num1 / num2;
+
+  allAnswers = [answer, dummy1, dummy2];
+
+  for (i = allAnswers.length; i--; ) {
+    switchAnswers.push(
+      allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
+    );
+  }
+
+  option1.innerHTML = switchAnswers[0];
+  option2.innerHTML = switchAnswers[1];
+  option3.innerHTML = switchAnswers[2];
+
+  //Update the content of the HTML element with id num1 & num2
+  document.getElementById("num1").innerHTML = num1;
+  document.getElementById("num2").innerHTML = num2;
+
+  console.log("num1:", num1);
+  console.log("num2:", num2);
+  console.log("answer:", answer);
+}
+
+generate_equation();
